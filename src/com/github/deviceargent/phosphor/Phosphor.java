4a6bf2a353ab52e6568ab446f9b6e0a69640cd59
deviceargent <deviceargent@users.noreply.github.com>
2026-08-22 16:41:15 +0000

    Fix package casing (phosphor lowercase) and add CI build for FlatPhosphor.jar

---
+++ src/com/github/deviceargent/phosphor/Phosphor.java
@@ -0,0 +1,22 @@
+package com.github.deviceargent.phosphor;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+
+public class Phosphor
+	extends FlatDarkLaf
+{
+	public static final String NAME = "Phosphor";
+
+	public static boolean setup() {
+		return setup( new Phosphor() );
+	}
+
+	public static void installLafInfo() {
+		installLafInfo( NAME, Phosphor.class );
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+}
